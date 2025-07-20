9c3344539da27c5216a14cd4a5c21ac77eb6f59e
WerWolv <werwolv98@gmail.com>
2025-07-20 21:33:15 +0200

    Fix build issues

--- include/screens/screen.hpp
+++ include/screens/screen.hpp
@@ -5,18 +5,18 @@
 #include <renderer/renderer.hpp>
 #include <renderer/fonts/font.hpp>
 
-namespace  {
+namespace obegransad {
 
     class Screen {
     public:
         virtual ~Screen() = default;
 
-        virtual const char *getName() const = 0;
+        [[nodiscard]] virtual const char *getName() const = 0;
         virtual void onShow() {}
         virtual void onHide() {}
 
-        virtual std::uint32_t getVisibleTime() const { return 30 * 1000;}
-        virtual std::uint32_t getUpdateInterval() const { return 0; }
+        [[nodiscard]] virtual std::uint32_t getVisibleTime() const { return 30 * 1000;}
+        [[nodiscard]] virtual std::uint32_t getUpdateInterval() const { return 0; }
 
         virtual void draw() = 0;
         virtual void onButtonPress() {}
